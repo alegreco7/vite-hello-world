14b8f41bdfe0084ce2d0596811586dcd1b6a08ad
alegreco7 <alexgreco2002@gmail.com>
2023-12-22 11:32:25 +0100

    content app hero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+<template lang="">
+    <div style="border: 1px solid black">
+        <h1>Jumbotron</h1>
+        <h3>Questo è VUE</h3>
+        <img src="../assets/vue.svg" alt="Logo">
+    </div>
+</template>
+<style lang="">
+    
+</style>
